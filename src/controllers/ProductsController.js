3622f053682db5539b604fd1756de469e30b4971
luis.freire <luis.freire@hemoce.ce.gov.br>
2023-07-24 13:26:03 -0300

    CRUD em desenvolvimento

--- src/controllers/ProductsController.js
+++ src/controllers/ProductsController.js
@@ -4,9 +4,51 @@ function ProductsController() {
     return {
 
         async getAllProducts(req, res) {
-            const products = await Products.findAll({ attributes: { exclude: ['createdAt', 'updatedAt', 'deletedAt'] }})
+            const products = await Products.findAll({ attributes: { exclude: ['createdAt', 'updatedAt', 'deletedAt'] } })
 
-            return res.json(products)
+            if(!products) return res.status(404).json({
+                status: 404,
+                message: 'Nenhum produto encontrado!'
+            })
+
+            return res.status(200).json({
+                status: 200,
+                produtos: products
+            })
+        },
+
+        async getProductById(req, res) {
+            const { productId: id } = req.params;
+
+            const product = await Products.findOne({ where: { id }, attributes: { exclude: ['createdAt', 'updatedAt', 'deletedAt'] } })
+
+            if(!product) return res.status(404).json({ status: 404, message: 'Produto não encontrado na base de dados'})
+        
+            return res.status(200).json({
+                status: 200,
+                produto: product,
+                path: req.originalUrl
+            })
+            
+        },
+
+        async createNewProduct(req, res) {
+            const { name, description, price } = req.body;
+
+            const [ product, created ] = await Products.findOrCreate({
+                where: { name },
+                defaults: { name, description, price },
+                attributes: { exclude: ['createdAt', 'updatedAt', 'deletedAt']}
+            })
+
+            if(!created) return res.status(400).json({ 
+                status: 400, 
+                message: "O produto já existe na base de dados!", 
+                path:`/produtos/${product.id}` ,
+                produto: product
+            })
+
+            return res.status(201).json({ status: 201, message: 'Produto cadastrado com sucesso!' })
         }
     }
 }
--- src/routes/Products.js
+++ src/routes/Products.js
@@ -3,5 +3,7 @@ const ProductsController = require('../controllers/ProductsController')
 
 router
     .get('/', ProductsController().getAllProducts)
+    .get('/:productId', ProductsController().getProductById)
+    .post('/', ProductsController().createNewProduct)
 
 module.exports = router;
